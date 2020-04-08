f9f520373a8ffd1485b500c5fe4f98e14fe99b18
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-08 17:26:58 +0200

    New SpringBoot Eureka Server Project

---
+++ src/test/java/com/joseluisestevez/webflux/eureka/SpringbootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.webflux.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
