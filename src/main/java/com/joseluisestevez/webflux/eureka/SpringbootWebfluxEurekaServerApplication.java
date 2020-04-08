6f10876882bed3c9781cf899fdb9831522e36261
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-08 17:37:22 +0200

    Configure Eureka Server

--- src/main/java/com/joseluisestevez/webflux/eureka/SpringbootWebfluxEurekaServerApplication.java
+++ src/main/java/com/joseluisestevez/webflux/eureka/SpringbootWebfluxEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.joseluisestevez.webflux.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringbootWebfluxEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebfluxEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebfluxEurekaServerApplication.class, args);
+    }
 
 }
